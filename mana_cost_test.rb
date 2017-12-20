60fb7aa4b8ee32ea3112ee274fb1d3d64f4e232e
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-12-20 14:58:52 +0900

    Add test case for Hybrid Mana Symbol

--- mana_cost_test.rb
+++ mana_cost_test.rb
@@ -28,8 +28,24 @@ class ManaCostTest < Test::Unit::TestCase
     end
 
     test 'Hybrid Mana Symbol' do
+      assert_true(ManaCost.convert('(白/青)') == 1)
+      assert_true(ManaCost.convert('(青/黒)') == 1)
+      assert_true(ManaCost.convert('(黒/赤)') == 1)
+      assert_true(ManaCost.convert('(赤/緑)') == 1)
+      assert_true(ManaCost.convert('(緑/白)') == 1)
+      assert_true(ManaCost.convert('(白/黒)') == 1)
+      assert_true(ManaCost.convert('(青/赤)') == 1)
+      assert_true(ManaCost.convert('(黒/緑)') == 1)
       assert_true(ManaCost.convert('(赤/白)') == 1)
+      assert_true(ManaCost.convert('(緑/青)') == 1)
+    end
+
+    test 'Hybrid Mana Symbol 2' do
       assert_true(ManaCost.convert('(2/白)') == 2)
+      assert_true(ManaCost.convert('(2/青)') == 2)
+      assert_true(ManaCost.convert('(2/黒)') == 2)
+      assert_true(ManaCost.convert('(2/赤)') == 2)
+      assert_true(ManaCost.convert('(2/緑)') == 2)
     end
 
     test 'Phyrexian Mana' do
